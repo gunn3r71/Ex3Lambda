73c886a24901ab50193ae6c8b196128f07b0e3df
Lucas <lucasgunner3@gmail.com>
2021-11-25 21:09:56 -0300

    Adicionar arquivos de projeto.

---
+++ Ex3Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            Console.WriteLine(Count(elements, x => x > 5));
+        }
+
+        static int Count(List<int> elements, Predicate<int> condition)
+        {
+            var i = 0;
+
+            elements.ForEach(x =>
+            {
+                if (condition(x))
+                    i++;
+            });
+
+            return i;
+        }
+    }
+}
